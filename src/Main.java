c8314932b4b825a528463835b27d01048331c5b6
Gideon <sgb07.gb@gmail.com>
2024-10-23 12:36:28 +0300

    initial commit message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("abcdefghijklmnopqrstuvwxyz");
+    }
+}
